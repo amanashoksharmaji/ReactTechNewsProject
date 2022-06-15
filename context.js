7f9fa3091b77a8b6fd2de087c3d51aa3e90acb29
amansharma <amanashok2001@gmail.com>
2022-06-16 02:10:23 +0530

    first commit

---
+++ context.js
@@ -0,0 +1,82 @@
+// context creation
+//  provider
+//  consumer instead use useContext renderHook
+//  useContext renderHook
+
+import React, { useContext, useEffect, useReducer } from "react";
+import reducer from "./reducer";
+
+let API = "http://hn.algolia.com/api/v1/search?";
+
+const initialState = {
+  isLoading: true,
+  query: "HTML",
+  nbPages: 0,
+  page: 0,
+  hits: [],
+};
+
+const AppContext = React.createContext();
+
+// to create a provider function
+
+const AppProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  const fetchApiData = async (url) => {
+    dispatch({ type: "SET_LOADING" });
+
+    try {
+      const res = await fetch(url);
+      const data = await res.json();
+      console.log(data);
+      dispatch({
+        type: "GET_STORIES",
+        payload: {
+          hits: data.hits,
+          nbPages: data.nbPages,
+        },
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  // to remove the post
+  const removePost = (post_ID) => {
+    dispatch({ type: "REMOVE_POST", payload: post_ID });
+  };
+
+  // search
+  const searchPost = (searchQuery) => {
+    dispatch({ type: "SEARCH_QUERY", payload: searchQuery });
+  };
+
+  // Pagination
+  const getNextPage = () => {
+    dispatch({ type: "NEXT_PAGE" });
+  };
+
+  const getPrevPage = () => {
+    dispatch({ type: "PREV_PAGE" });
+  };
+
+  // to call the api function
+  useEffect(() => {
+    fetchApiData(`${API}query=${state.query}&page=${state.page}`);
+  }, [state.query, state.page]);
+
+  return (
+    <AppContext.Provider
+      value={{ ...state, removePost, searchPost, getNextPage, getPrevPage }}>
+      {children}
+    </AppContext.Provider>
+  );
+};
+
+// custom hook create
+const useGlobalContext = () => {
+  return useContext(AppContext);
+};
+
+export { AppContext, AppProvider, useGlobalContext };
